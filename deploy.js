063d147c0180c6c309ead88cabbb0238ebab0c01
gjuoun <gjuoun@gmail.com>
2020-06-15 02:44:40 -0300

    update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,16 +3,28 @@ const fs = require("fs")
 const ncp = require("ncp").ncp
 
 // copy /public to /docs
-fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
-fs.mkdirSync("docs")
+function deploy() {
+  fs.rmdir(
+    path.join(__dirname, "docs"),
+    { recursive: true, retryDelay: 500 },
+    err => {
+      if (!err) {
+        fs.mkdirSync("docs")
+        ncp(
+          path.join(__dirname, "public"),
+          path.join(__dirname, "docs"),
+          function (err) {
+            if (err) {
+              return console.error(err)
+            }
+            console.log("done!")
+          }
+        )
+      }
+    }
+  )
 
-ncp.limit = 16
+  ncp.limit = 16
+}
 
-ncp(path.join(__dirname, "public"), path.join(__dirname, "docs"), function (
-  err
-) {
-  if (err) {
-    return console.error(err)
-  }
-  console.log("done!")
-})
+deploy()
